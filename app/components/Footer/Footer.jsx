c1d74c1213f7d08f348a5493c9aeb6e4cd66fec7
Raziel <robroblemol>
2017-12-21 18:03:15 -0500

    set Footer with scss

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+//Dependencies
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const Footer = () =>
-  (
-    <div>
-      <h3>
-        This is the Footer
-      </h3>
-    </div>
-  );
+//Assets
+import './Footer.scss';
+
+class Footer extends Component {
+  static proptypes = {
+      copyrigth: PropTypes.string  };
+
+  render() {
+      const {copyrigth = '&copy; React 2017'} = this.props;
+      const {authors = ' by Nelson Diaz - Roberto Robles'} = this.props;
+
+    return (
+      <div className="Footer">
+        <p dangerouslySetInnerHTML={{ __html: copyrigth }} />
+        <p dangerouslySetInnerHTML={{ __html: authors }} />
+      </div>
+    );
+  }
+}
 
 export default Footer;
